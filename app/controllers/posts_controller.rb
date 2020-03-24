a11fc26918d8d3a5ba5a0626b3bd391f82185346
holy2943 <59240738+holy2943@users.noreply.github.com>
2020-03-24 22:35:00 +0900

    mergeメソッドの追記

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content,).merge(user_id: current_user.id)
   end
 
 
